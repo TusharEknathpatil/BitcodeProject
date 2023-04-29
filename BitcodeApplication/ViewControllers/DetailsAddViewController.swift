95deaf203c471924d282c0f9d7bfdc07f7bc41e8
Tushar Patil <122349237+TusharEknathpatil@users.noreply.github.com>
2023-04-29 12:23:06 +0530

    Add files via upload
    Add UiAletButton And Use Gaurd Let

--- BitcodeApplication/ViewControllers/DetailsAddViewController.swift
+++ BitcodeApplication/ViewControllers/DetailsAddViewController.swift
@@ -22,9 +22,20 @@ class DetailsAddViewController: UIViewController {
     
 
     @IBAction func addDetailsButton(_ sender: Any) {
-        var name = nameTextField.text
-        var age = ageTextField.text
-        var objectOfStudent = Student(name: name!, age: (age?.codingKey.intValue)!)
+        guard let name = nameTextField.text,
+        let age = ageTextField.text?.codingKey.intValue
+        else {
+            
+            let uiAlertController = UIAlertController(title: "EmptyData", message: "Enter A Data", preferredStyle: .alert)
+            let uiAlertAction = UIAlertAction(title: "Ok", style: .default)
+            uiAlertController.addAction(uiAlertAction)
+            present(uiAlertController, animated: true)
+            
+            return  }
+        
+//        var name = nameTextField.text
+//        var age = ageTextField.text
+        var objectOfStudent = Student(name: name, age: (age.codingKey.intValue!))
         guard let delegateSVC = delegate else { return  }
         delegateSVC.backDataPassing(student: objectOfStudent)
         navigationController?.popViewController(animated: true)
